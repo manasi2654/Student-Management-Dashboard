4559df282a3f8885d95a6cd61e463bc313c88636
manasi2654 <patilmansi2652004@gmail.com>
2025-09-08 21:01:57 +0530

    Initial commit - Student Management Dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.VITE_BASE_PATH || '/react-vite-deploy', // 👈 
+  base: process.env.VITE_BASE_PATH || '/react-vite-deploy', 
 });
